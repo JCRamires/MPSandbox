fdcd7abb6687f35de62f4ded55ff1d11abc8f6e5
Juan Carlos Ramires <jcramires851@gmail.com>
2015-09-17 18:07:18 -0300

    alterações

--- jctest/polls/models.py
+++ jctest/polls/models.py
@@ -1,8 +1,12 @@
+from datetime import datetime
 from django.db import models
 
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    def was_published_today(self):
+        return self.pub_date.date() == datetime.date.today()
+    was_published_today.short_description = 'Published today?'
     def __unicode__(self):
         return self.question
 
